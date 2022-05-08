0b1f3dda0ef2d4b84ba25457917d247bbcf954f8
Lim Rona <limrona97@gmail.com>
2022-05-08 17:26:21 +0800

    first

---
+++ src/main/java/vttp2022/paf/EcommerceStore/EcommerceStoreApplication.java
@@ -0,0 +1,13 @@
+package vttp2022.paf.EcommerceStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceStoreApplication.class, args);
+	}
+
+}
